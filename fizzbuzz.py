ffffd7bbf4da2efadca0ce1de0826ad1ea06d9fe
wldwjd <wlswjd010629@gmail.com>
2025-04-23 10:17:26 +0900

    feat: Print buzz Print fizzbuzz if i is multiple of 3

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,5 +1,7 @@
-for i in range(1, 17):
-    if i % 3  == 0:
+for i in range(1, 18):
+    if i % 15 == 0:
         print('fizz')
+    elif i % 3 == 0:
+        print(('fizzbuzz')
     else:
         print(f'{i}')
