6fea0e76356b45a7ede18ffd44bb8f872ffd4373
wldwjd <wlswjd010629@gmail.com>
2025-04-23 11:13:25 +0900

    rafactor: fizzbuzz with i if statement

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,9 +1,5 @@
 for i in range(1, 18):
-    if i % 15 == 0:
-        print('fizz')
-    elif i % 3 == 0:
-        print(('fizzbuzz')
-    elif i % 5 == 0:
-        print('buzz')
+    if i % 3 == 0 or i % 5 == 0:
+        print('fizz'*(i%3==0) + 'Buzz'*(i%5==0))
     else:
         print(f'{i}')
